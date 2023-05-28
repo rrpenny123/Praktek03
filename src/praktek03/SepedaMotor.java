e04b12cec20c9251d6fef216b1f807f4d568f66c
rrpenny123 <penni.app123@gmail.com>
2023-05-28 23:51:52 +0800

    Attribute dan Method

--- src/praktek03/SepedaMotor.java
+++ src/praktek03/SepedaMotor.java
@@ -1,5 +1,10 @@
 package praktek03;
 
 public class SepedaMotor extends Sepeda {
+    private String jenisMotor;
+    
+    void throttle() {
+        System.out.println("NGEEEEEENG.........");
+    }
     
 }
