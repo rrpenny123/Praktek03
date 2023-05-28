291a02af443397e0fcb652e06a1c202c91fac17c
rrpenny123 <penni.app123@gmail.com>
2023-05-28 23:52:13 +0800

    Getter and Setter

--- src/praktek03/SepedaMotor.java
+++ src/praktek03/SepedaMotor.java
@@ -6,5 +6,15 @@ public class SepedaMotor extends Sepeda {
     void throttle() {
         System.out.println("NGEEEEEENG.........");
     }
+
+    public String getJenisMotor() {
+        return jenisMotor;
+    }
+
+    public void setJenisMotor(String jenisMotor) {
+        this.jenisMotor = jenisMotor;
+    }
+    
+    
     
 }
